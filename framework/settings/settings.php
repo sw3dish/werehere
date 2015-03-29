c6117aef806cf3c73cdef8e149cb3f86074cbd7c
Colin Burr <cburr1014@gmail.com>
2015-03-29 16:02:13 -0400

    add settings folder

---
+++ framework/settings/settings.php
@@ -0,0 +1,13 @@
+<?php
+$default_core_settings = array(
+	"db_connection" => array(
+		"name" =>  'were_here'
+		,"host" => 'localhost'
+		,"user" => 'root'
+		,"password" => ''
+		,"die_on_connection_failure" => true
+		,"connection_error_message" => "The system is currently not accessible."
+		,"email_on_connection_failure" => true
+	)
+);
+?>
